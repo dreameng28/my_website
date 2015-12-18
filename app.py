493ce4799db442e537f8e4bb25aed602a66016cc
dreameng <dreameng@dreamengdeMacBook-Air.local>
2015-12-18 20:35:55 +0800

    界面大改了

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, session, abort, flash, redirect, url_for, g
 import leancloud
 from leancloud import Object
 import os
@@ -15,25 +15,38 @@ class ClickInfo(Object):
     pass
 
 @app.route('/')
-def index():
-    ip = request.remote_addr
-    agent = str(request.user_agent)
-    click_info = ClickInfo()
-    click_info.set('click_ip', ip)
-    click_info.set('click_agent', agent)
-    if 'Android' in agent:
-        system = 'Android'
-    elif 'Macintosh' in agent:
-        system = 'Mac'
-    elif 'iPhone' in agent:
-        system = 'iPhone'
-    elif 'windows' or 'Windows' in agent:
-        system = 'Windows'
-    else:
-        system = 'other'
-    click_info.set('system', system)
-    click_info.save()
-    query = leancloud.Query(ClickInfo)
-    count = query.count()
-    return render_template('index.html', count=count, info=system)
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
